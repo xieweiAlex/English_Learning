c2d74e11cc0b40380f3262419c777ab02171abc9
Alex Xie <xieweialex@gmail.com>
2019-01-13 12:35:28 -0800

    Add script to filter and sync - words

---
+++ sync.sh
@@ -0,0 +1,8 @@
+
+rm word-review.md
+
+ag '\*{2,}.*\s-{1,3}\W{0,10}$' --group --nonumbers > word-review.md
+
+echo "\n\n ------- Second part ----- \n" >> word-review.md 
+ag '\*{2,}.*\s-{2,3}\W{0,10}$' --group --nonumbers >> word-review.md
+
