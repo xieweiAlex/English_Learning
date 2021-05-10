c85a20d6411642dbbc6dcbeac85cd29777fdace8
Alex Xie <alex.xie2016+dotfile@gmail.com>
2021-05-09 17:30:36 -0700

    Changes at: 05/09/2021 05:30:36 PM

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,15 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Commit changes.
+# Default msg as current time 
 date=$(date +'%m/%d/%Y %r')
 msg="Changes at: $date"
-
-echo "$msg \n\n"
 
 if [ -n "$*" ]; then
 	msg="$*"
 fi
+echo "$msg \n\n"
+
 git commit -m "$msg"
 
 # Push source and build repos.
